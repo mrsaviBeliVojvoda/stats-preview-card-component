f63a59da1e87d9dbf28f048e02a6314104e05fb0
mrsaviBeliVojvoda <149872278+mrsaviBeliVojvoda@users.noreply.github.com>
2024-04-22 19:29:24 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        mainbg: '#090b1a',
+        secbg: '#1b1938',
+        softviolet: '#aa5cdb',
+      },
+    },
+  },
+  plugins: [],
+};
